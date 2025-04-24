d8691d659079e69aa5a15cf04425b9226997477d
SimonSaputra29 <simonsaputra2908@gmail.com>
2025-04-24 09:25:12 +0700

    role

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -13,8 +14,15 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $role): Response
     {
+        $user = Auth::user();
+
+        // Jika tidak login atau role tidak sesuai
+        if (!$user || $user->role !== $role) {
+            abort(403, 'Akses ditolak.');
+        }
+
         return $next($request);
     }
 }
